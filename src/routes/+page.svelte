01e1dbe3f3dc7a5f122aa9255aef444cc8a485ba
0xmiki <0xmik@proton.me>
2025-10-21 18:40:41 +0300

    update blog

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -11,55 +11,23 @@
 	<div class=" prose prose-neutral dark:prose-invert prose-base mt-55 w-full">
 		<TheMissingPiece />
 	</div>
-	<!-- <div class="mt-55">
-		<p class="text-3xl">The Missing Piece</p>
-		<div class="mt-3">
-			<li class="ml-6">Most of AI inference will be used for generating videos and images</li>
-			<li class="ml-6">Everyone is competing to build nanobanana wrappers, however</li>
-			<li class="ml-12">
-				No one is building the infrastructure for devs. The "vercel", the one click deploy
-				experience.
-			</li>
-			<li class="ml-12">
-				No one is rethinking the user experience. The "cursor", like how programmers went from copy
-				pasting from chatgpt to using cursor.
-			</li>
-			<li class="ml-6">And that's exactly what I am building</li>
-			<li class="ml-12">
-				A node based canvas like Excalidraw to visually construct media generation piplines
-			</li>
-			<li class="ml-12">
-				And a backend infra which handles image processing and compression as well as video delivery
-			</li>
-			<li class="ml-6">I faced two problems while building my own ai media app</li>
-			<li class="ml-12">
-				Image generation pipelines got too messy and complicated to manage as code
-			</li>
-			<li class="ml-12">
-				I had to figure out image / video storage and retreval and on top of that I had to compress
-				the generated images to display them faster.
-			</li>
-			<li class="ml-6">The Solution</li>
-			<li class="ml-12">
-				Node based canvas to visually construct media generation pipelines. Think of it as designing
-				prompts and entire pipelines in Excalidraw
-			</li>
-			<li class="ml-12">
-				An API that connects to media generation models like nanobanana and builds a CDN layer on
-				top of them. All devs have to do now is call the model through API and they get media
-				delivery, compression and storage out of the box.
-			</li>
-			<li class="ml-24">
-				Simplifying infrastructure is a crucial step in allowing anyone to launch their own niche
-				Media generation apps. Especially now with the rise of vibe coding and viral marketing.
-				Creators want to build their own apps for their followers. The API empower them when plugged
-				into vibe coding tools.
-			</li>
-		</div>
-	</div> -->
 	<div class="mt-55 flex flex-col">
-		<a target="_blank" href="https://github.com/0xmiki" class="hover:underline">GitHub</a>
-		<a target="_blank" href="https://t.me/willtocode" class="hover:underline">Telegram channel</a>
-		<a target="_blank" href="https://x.com/miki_code" class="hover:underline">X account</a>
+		<a
+			target="_blank"
+			href="https://github.com/0xmiki"
+			class="opacity-80 hover:underline hover:opacity-90">GitHub</a
+		>
+		<a
+			target="_blank"
+			href="https://x.com/miki_code"
+			class="opacity-80 hover:underline hover:opacity-90">X account</a
+		>
+
+		<a
+			target="_blank"
+			href="https://t.me/willtocode"
+			class="opacity-80 hover:underline hover:opacity-90">Telegram channel</a
+		>
+		<p class="opacity-80 hover:opacity-90">mikiyas.dev@gmail.com</p>
 	</div>
 </div>
