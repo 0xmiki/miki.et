02d6d39fd2d5ba613eb8fcb934f488f2d4ce1e90
0xmiki <0xmik@proton.me>
2025-10-21 07:03:56 +0300

    update 1

---
+++ src/lib/components/hover.svelte
@@ -0,0 +1,35 @@
+<script lang="ts">
+	import { Button } from '$lib/components/ui/button/index.js';
+	import { Label } from '$lib/components/ui/label/index.js';
+	import { Input } from '$lib/components/ui/input/index.js';
+	import * as Card from '$lib/components/ui/card/index.js';
+	import * as HoverCard from '$lib/components/ui/hover-card/index.js';
+
+	let { hover, title = null, description, footer = null } = $props();
+</script>
+
+<HoverCard.Root>
+	<HoverCard.Trigger
+		href="https://github.com/sveltejs"
+		target="_blank"
+		rel="noreferrer noopener"
+		class="rounded-sm  text-blue-400"
+	>
+		{hover}
+	</HoverCard.Trigger>
+	<HoverCard.Content class="w-80">
+		<div class="flex justify-between space-x-4">
+			<div class="space-y-1">
+				{#if title}
+					<h4 class=" text-sm font-semibold">{title}</h4>
+				{/if}
+				<p class="text-sm">{description}</p>
+				{#if footer}
+					<div class="flex items-center pt-2">
+						<span class="text-muted-foreground text-xs"> {footer} </span>
+					</div>
+				{/if}
+			</div>
+		</div>
+	</HoverCard.Content>
+</HoverCard.Root>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,19 +1,21 @@
 <script lang="ts">
-	import { Button } from '$lib/components/ui/button/index.js';
-	import { Label } from '$lib/components/ui/label/index.js';
-	import { Input } from '$lib/components/ui/input/index.js';
-	import * as Card from '$lib/components/ui/card/index.js';
-	import * as HoverCard from '$lib/components/ui/hover-card/index.js';
+	import Hover from '$lib/components/hover.svelte';
 </script>
 
-<div class="p-[15vw]">
+<div class="p-[9vw] lg:p-[15vw]">
 	<div class="px-6">
+		<li>21 yo</li>
 		<li>e/acc maxi</li>
-		<li>agi will not happen unless I build it</li>
+		<li>agi will not happen (unless I build it)</li>
 		<li>llm engineering = applied alchemy</li>
 		<li>believer in spontaneous creativity</li>
-		<li>can build full stack applications (trust me bro)</li>
-		<li>enough talk i'm currently building the "vercel" and "notion" for AI generated media</li>
+		<li>
+			can build full stack applications. <Hover
+				hover={'(trust me bro)'}
+				description="Pinkie promise"
+			/>
+		</li>
+		<li>currently building the "vercel" and the "notion" for AI generated media</li>
 	</div>
 	<div class="mt-55">
 		<p class="text-3xl">The Missing Piece</p>
@@ -29,9 +31,11 @@
 				pasting from chatgpt to using cursor.
 			</li>
 			<li class="ml-6">And that's exactly what I am building</li>
-			<li class="ml-12">A node based canvas to visually construct media generation piplines</li>
 			<li class="ml-12">
-				And a backend infra which handles image processing and compression as well as video
+				A node based canvas like Excalidraw to visually construct media generation piplines
+			</li>
+			<li class="ml-12">
+				And a backend infra which handles image processing and compression as well as video delivery
 			</li>
 			<li class="ml-6">I faced two problems while building my own ai media app</li>
 			<li class="ml-12">
@@ -42,31 +46,19 @@
 				the generated images to display them faster.
 			</li>
 			<li class="ml-6">The Solution</li>
-			<li class="ml-12">Node based canvas to visually construct media generation pipelines</li>
 			<li class="ml-12">
-				An API with a CDN built in. All devs have to do now is use my API and they get media
-				compression and storage out of the box.
-				<HoverCard.Root>
-					<HoverCard.Trigger
-						href="https://github.com/sveltejs"
-						target="_blank"
-						rel="noreferrer noopener"
-						class="rounded-sm underline  focus-visible:outline-2 focus-visible:outline-offset-8 focus-visible:outline-black"
-					>
-						@sveltejs
-					</HoverCard.Trigger>
-					<HoverCard.Content class="w-80">
-						<div class="flex justify-between space-x-4">
-							<div class="space-y-1">
-								<h4 class=" text-sm font-semibold">@sveltejs</h4>
-								<p class="text-sm">Cybernetically enhanced web apps.</p>
-								<div class="flex items-center pt-2">
-									<span class="text-muted-foreground text-xs"> Joined September 2022 </span>
-								</div>
-							</div>
-						</div>
-					</HoverCard.Content>
-				</HoverCard.Root>
+				Node based canvas to visually construct media generation pipelines. Think of it as designing
+				prompts and entire pipelines in Excalidraw
+			</li>
+			<li class="ml-12">
+				An API that connects to media generation models like nanobanana and builds a CDN layer on
+				top of them. All devs have to do now is call the model through API and they get media
+				delivery, compression and storage out of the box.
+			</li>
+			<li class="ml-24">
+				Simplifying infrastructure is a crucial step in allowing anyone to launch their own niche
+				Media generation apps. Especially now with the rise of vibe coding and viral marketing.
+				Creators want to build their own apps for their followers.
 			</li>
 		</div>
 	</div>
